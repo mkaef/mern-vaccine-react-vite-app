fdf3bfff3abf3bc205ecbd3950c2d20985a75312
mkaef <eric_franck2@yahoo.fr>
2024-07-19 08:34:28 -0500

    vaccination

---
+++ backend/src/models/vaccine.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { schemaOptions } = require('./modelOptions');
+
+const vaccineShema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  schemaOptions
+);
+
+module.exports = mongoose.model('Vaccine', vaccineShema);
